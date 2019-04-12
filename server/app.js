c1f8be7e60466387bc7cfa13112569d5f95e3018
ImamMatot <imammatot@gmail.com>
2019-04-12 15:02:23 +0700

    add medsos on web

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const cors = require('cors')
+const weddingRoutes = require('./routes/wedding')
 
 const port = 4000
 
@@ -11,6 +12,9 @@ mongoose.connect('mongodb://localhost:27017/weddingInvitation', { useNewUrlParse
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+
+app.use('/', weddingRoutes)
 
 
 app.listen(port, () => {
